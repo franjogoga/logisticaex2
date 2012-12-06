956c6ed9b44d41d705854e87934d58b85afd1013
cbustamanteaponte <cbustamanteaponte@d70edf37-ed83-2052-883a-5ee09ef9a601>
2012-12-06 04:34:30 +0000

    fuentes

--- LogisticaSystemEX2/src/logisticasystemex2/LogisticaSystemEX2.java
+++ LogisticaSystemEX2/src/logisticasystemex2/LogisticaSystemEX2.java
@@ -15,5 +15,6 @@ public class LogisticaSystemEX2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Victor y Dorita estarán in love ");
     }
 }
